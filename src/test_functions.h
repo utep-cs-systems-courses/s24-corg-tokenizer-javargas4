4da4c055bc90e4ea8b3d4f7ae67be4fca0a5196e
javargas4 <javargas4@miners.utep.edu>
2024-03-23 08:22:52 -0600

    Created test_functions.c to organize code

---
+++ src/test_functions.h
@@ -0,0 +1,14 @@
+#ifndef TEST_FUNCTIONS_H
+
+#define TEST_FUNCTIONS_H
+
+void test_space_char();
+void test_non_space_char();
+void test_token_start();
+void test_token_terminator();
+void test_count_tokens();
+void test_copy_str();
+
+
+#endif 
+
