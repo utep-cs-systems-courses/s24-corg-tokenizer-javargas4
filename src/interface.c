bf942cc8b72ebd0a4bd9fcf9e059aa193fdead54
javargas4 <javargas4@miners.utep.edu>
2024-03-22 18:29:59 -0600

    Created/Tested non_space_char(); It works

--- src/interface.c
+++ src/interface.c
@@ -1,5 +1,45 @@
 #include "tokenizer.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+void test_space_char () {
+  char test_letter = ' ';
+  printf("int space_char return value with test c = ' '\nReturn: %d\n", space_char(test_letter));
+
+  test_letter = '\t';
+  printf("int space_char return value with test c = '\\t'\nReturn: %d\n", space_char(test_letter));
+
+  test_letter = 'a';
+  printf("int space_char return value with test c = 'a'\nReturn: %d\n", space_char(test_letter));
+
+  test_letter = 'A';
+  printf("int space_char return value with test c = 'A'\nReturn: %d\n", space_char(test_letter));
+
+}
+
+void test_non_space_char () {
+  char test_letter = ' ';
+  printf("int non_space_char return value with test c = ' '\nReturn: %d\n",
+	 non_space_char(test_letter));
+
+  test_letter = '\t';
+  printf("int non_space_char return value with test c = '\\t'\nReturn: %d\n",
+	 non_space_char(test_letter));
+
+  test_letter = '\0';
+  printf("int non_space_char return value with test c = '\\0'\nReturn: %d\n",
+	 non_space_char(test_letter));
+
+  test_letter = 'a';
+  printf("int non_space_char return value with test c = 'a'\nReturn: %d\n",
+	 non_space_char(test_letter));
+
+  test_letter = 'A';
+  printf("int non_space_char return value with test c = 'A'\nReturn: %d\n",
+	 non_space_char(test_letter));
+
+}
+
 
 // Basic UI
 int main (void) {
@@ -16,13 +56,7 @@ int main (void) {
   // print user input
   printf("%s", user_input);
   */
-  char test_letter = ' ';
-  printf("int space_char return value with test c = ' '\nReturn: %d\n", space_char(test_letter));
-
-  test_letter = '\t';
-  printf("int space_char return value with test c = '\t'\nReturn: %d\n", space_char(test_letter));
-
-  test_letter = 'y';
-  printf("int space_char return value with test c = 'y'\nReturn: %d\n", space_char(test_letter));
-
+  test_space_char();
+  printf("\n");
+  test_non_space_char();
 }
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,5 +1,7 @@
 #include "tokenizer.h"
 #include <stdio.h>
+#include <stdlib.h>
+
 
 /* Return true (non-zero) if c is a whitespace character
    ('\t' or ' ').
@@ -7,4 +9,13 @@
 int space_char (char c) {
   return c == ' ' || c == '\t';
   
+}
+
+
+/* Return true (non-zero) if c is a non-whitespace 
+   character (not tab or space).  
+   Zero terminators are not printable (therefore false) */ 
+int non_space_char (char c) {
+  return c != ' ' && c != '\t' && c != '\0';
+
 }
